0594306befc7ebc90b1dd68ac2eb04ad0ec1869c
VincentNdegwa <116282941+VincentNdegwa@users.noreply.github.com>
2023-05-03 02:02:33 +0300

    my project

--- src/componets/HeroPage/HeroPage.jsx
+++ src/componets/HeroPage/HeroPage.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import "./HeroPage.css";
+import { motion } from "framer-motion";
 
 function HeroPage() {
   return (
-    <div className="hero-page">
-      <div className="hero-details">
-        <div className="hero-item">
-          <div className="hero-name">
-            <span>Vin</span>cent Nde<span className="span-name2">gwa</span>
-          </div>
-
-          <div className="hero-skills">
-            <div>
-              Skilled Software Engineer with a Passion for Front-End Web
-              Development and User-Centered Design
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+    >
+      <div className="hero-page">
+        <div className="hero-details">
+          <div className="hero-item">
+            <div className="hero-name">
+              <span>Vin</span>cent Nde<span className="span-name2">gwa</span>
             </div>
 
-            <p>
-              Solving problems and simplifying the complex through elegant
-              design and coding
-            </p>
+            <div className="hero-skills">
+              <div>
+                Skilled Software Engineer with a Passion for Front-End Web
+                Development and User-Centered Design
+              </div>
+
+              <p>
+                Solving problems and simplifying the complex through elegant
+                design and coding
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- src/componets/Projects/Projects.jsx
+++ src/componets/Projects/Projects.jsx
@@ -3,51 +3,60 @@ import "./Projects.css";
 import ProjecData from "../../Data/ProjectData";
 import { AiOutlineGithub } from "react-icons/ai";
 import { CiStreamOn } from "react-icons/ci";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Projects() {
   return (
-    <div className="Projects" id="Projects">
-      <header>Projects</header>
-      <div className="Projects-container">
-        {ProjecData.map((item, index) => {
-          return (
-            <div className="Projects-item-holder" key={index}>
-              <div className="Project-card">
-                <div className="Project-image">
-                  <img src={item.projectImg} alt="playtube" />
-                </div>
-                <div className="Project-name">
-                  <h2>{item.projectName}</h2>
-                </div>
-                <div className="Project-github-live">
-                  <div className="Project-github">
-                    <a
-                      href={item.projectGithub}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <AiOutlineGithub />
-                    </a>
-                  </div>
+    <AnimatePresence>
+      <div className="Projects" id="Projects">
+        <header>Projects</header>
+        <div className="Projects-container">
+          {ProjecData.map((item, index) => {
+            return (
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+              >
+                <div className="Projects-item-holder" key={index}>
+                  <div className="Project-card">
+                    <div className="Project-image">
+                      <img src={item.projectImg} alt="playtube" />
+                    </div>
+                    <div className="Project-name">
+                      <h2>{item.projectName}</h2>
+                    </div>
+                    <div className="Project-github-live">
+                      <div className="Project-github">
+                        <a
+                          href={item.projectGithub}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <AiOutlineGithub />
+                        </a>
+                      </div>
 
-                  <div className="Project-live">
-                    <a
-                      href={item.projecLive}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <CiStreamOn />
-                    </a>
+                      <div className="Project-live">
+                        <a
+                          href={item.projecLive}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <CiStreamOn />
+                        </a>
+                      </div>
+                    </div>
+                    <div className="Project-description">
+                      <p>{item.projectDescription}</p>
+                    </div>
                   </div>
                 </div>
-                <div className="Project-description">
-                  <p>{item.projectDescription}</p>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+              </motion.div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </AnimatePresence>
   );
 }
